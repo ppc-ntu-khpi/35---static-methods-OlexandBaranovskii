5bb67fb6eb7da6f8c90db45aa8b7cd03259ebe25
OlexandBaranovskii <72404610+OlexandBaranovskii@users.noreply.github.com>
2021-02-10 21:03:25 +0300

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,19 +1,1 @@
-package test;
 
-import java.util.Scanner;
-import domain.Exercise;
-
-public class TestResult {
-    public static void main(String[] args){
-        System.out.println("Enter the string: ");
-        
-        Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine();
-       
-        System.out.println("Encrypted string:");
-        String encriptedString = Cipher(inputString);
-        
-        System.out.println("Decrypted string:");
-        Cipher(encriptedString);
-    }
-}
