4b6497c5aeabe7682ed1864e09489d4aadcc13ad
OlexandBaranovskii <72404610+OlexandBaranovskii@users.noreply.github.com>
2021-02-10 20:50:39 +0300

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,19 @@
 package test;
 
+import java.util.Scanner;
 import domain.Exercise;
 
 public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    public static void main(String[] args){
+        System.out.println("Enter the string: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+       
+        System.out.println("Encrypted string:");
+        String encriptedString = Cipher(inputString);
+        
+        System.out.println("Decrypted string:");
+        Cipher(encriptedString);
     }
 }
