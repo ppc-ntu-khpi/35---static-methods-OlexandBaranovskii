22695183467ddf264adaa3bd1b0af9e05fb6ef83
OlexandBaranovskii <72404610+OlexandBaranovskii@users.noreply.github.com>
2021-02-10 21:12:27 +0300

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,1 +1,29 @@
 
+package domain;
+/**
+ * A class that represent an exercise
+ * @author DevShade
+ */
+public class Exercise {
+    
+    /**
+     * Method that encrypts entered string
+     * @param inputString entered string
+     * @return encrypted string
+     */
+    public static String Cipher(String inputString)
+    {
+        char key = 'L';
+        
+        String outputString = "";
+        
+        int length = inputString.length();
+        
+        for(int i = 0; i < length; i++)
+        {
+            outputString += Character.toString((char)(inputString.charAt(i)^key));
+        }
+        System.out.println(outputString);
+        return outputString;
+    }
+}
